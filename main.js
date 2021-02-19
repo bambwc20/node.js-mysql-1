de1a77093edf638e9bef878e40d188b5eb486a93
bambwc20 <bambwc20@naver.com>
2021-02-20 02:32:00 +0900

    2. 저자CRUD 완성

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 var http = require("http");
 var url = require("url");
-var qs = require("querystring");
-var template = require("./lib/template.js");
-var db = require("./lib/db");
 var topic = require("./lib/topic");
+var author = require("./lib/author");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -18,13 +16,23 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === "/create") {
     topic.create(request, response);
   } else if (pathname === "/create_process") {
-    topic.creeate_process(request, response);
+    topic.create_process(request, response);
   } else if (pathname === "/update") {
     topic.update(request, response);
   } else if (pathname === "/update_process") {
     topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
     topic.delete_process(request, response);
+  } else if (pathname === "/author") {
+    author.home(request, response);
+  } else if (pathname === "/author/create_process") {
+    author.create_process(request, response);
+  } else if (pathname === "/author/update") {
+    author.update(request, response);
+  } else if (pathname === "/author/update_process") {
+    author.update_process(request, response);
+  } else if (pathname === "/author/delete_process") {
+    author.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
